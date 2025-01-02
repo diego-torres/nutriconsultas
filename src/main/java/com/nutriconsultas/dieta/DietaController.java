3dfb30f0870e2302c4cf021f39407d9168e25ee5
Diego Alejandro Torres Fuerte <dtorresf@redhat.com>
2025-01-01 20:44:34 -0700

    change dieta name completed

--- src/main/java/com/nutriconsultas/dieta/DietaController.java
+++ src/main/java/com/nutriconsultas/dieta/DietaController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nutriconsultas.controller.AbstractAuthorizedController;
@@ -66,6 +68,15 @@ public class DietaController extends AbstractAuthorizedController {
     model.addAttribute("platillos", new ArrayList<PlatilloIngesta>());
 
     return "sbadmin/dietas/formulario";
+  }
+
+  @PostMapping(path = "/admin/dietas/save")
+  public String saveDieta(@RequestParam @NonNull Long id, @RequestParam @NonNull String nombre, Model model) {
+    logger.debug("Guardar dieta with id {}, {}", id, nombre);
+    Dieta _dieta = dietaService.getDieta(id);
+    _dieta.setNombre(nombre);
+    dietaService.saveDieta(_dieta);
+    return "redirect:/admin/dietas/" + id;
   }
   
 }
