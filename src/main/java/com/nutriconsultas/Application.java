24de9bb04267ce8fe9469697f87c3b04de0cec90
Diego Alejandro Torres Fuerte <dtorresf@redhat.com>
2025-12-29 14:01:31 -0700

    Remove utility constructor from Application class to prevent instantiation, simplifying the code structure.

--- src/main/java/com/nutriconsultas/Application.java
+++ src/main/java/com/nutriconsultas/Application.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	private Application() {
-		// Utility class - prevent instantiation
-	}
-
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
